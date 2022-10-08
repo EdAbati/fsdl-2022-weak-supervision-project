c756d4ac152de353072f1a82400dc95335fcfe62
Diego Quintana <daquintanav@gmail.com>
2022-10-08 12:09:52 +0200

    fix CLI and pyproject.toml

---
+++ services/streamlit-ui/src/app/cli.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+import typer
+
+from app.config import SettingsModeEnum, settings
+
+app = typer.Typer()
+
+
+@app.command()
+def greet(name: str = typer.Argument("FSDL", help="The name to greet")):
+    typer.echo(f"Hello, {name}!")
+
+
+@app.command()
+def print_settings():
+    color = typer.colors.BLUE
+    bold = False
+
+    for k, v in settings:
+        if isinstance(v, list):
+            typer.secho(f"{k}:", color=color, bold=bold)
+            for e in v:
+                typer.secho(f"\t{e}", color=color, bold=bold)
+        else:
+            typer.secho(f"{k}: {v}", color=color, bold=bold)
+
+
+if __name__ == "__main__":
+    app()
