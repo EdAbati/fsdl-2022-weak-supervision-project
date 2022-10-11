d2f9cd02ea695ae55cc0d9fd0ffe52843aae8f91
Edoardo Abati <29585319+EdAbati@users.noreply.github.com>
2022-10-11 21:51:02 +0000

    updated load_data

--- app/data.py
+++ app/data.py
@@ -1,7 +1,11 @@
+from typing import Union
 from datasets import ClassLabel, Features, Value, load_dataset
 
 
-def load_data(dataset_name: str = "bergr7/weakly_supervised_ag_news") -> tuple:
+def load_data(
+    dataset_name: str = "bergr7/weakly_supervised_ag_news",
+    split: Union[str, None] = None,
+) -> tuple:
     # files
     labeled_data_files = {
         "train": "train.csv",
@@ -19,26 +23,12 @@ def load_data(dataset_name: str = "bergr7/weakly_supervised_ag_news") -> tuple:
         }
     )
     # load data
-    labeled_dataset_train = load_dataset(
+    return load_dataset(
         dataset_name,
         data_files=labeled_data_files,
         features=labeled_features,
-        split="train",
+        split=split,
     )
-    labeled_dataset_val = load_dataset(
-        dataset_name,
-        data_files=labeled_data_files,
-        features=labeled_features,
-        split="validation",
-    )
-    labeled_dataset_test = load_dataset(
-        dataset_name,
-        data_files=labeled_data_files,
-        features=labeled_features,
-        split="test",
-    )
-
-    return labeled_dataset_train, labeled_dataset_val, labeled_dataset_test
 
 
 def load_unlabeled_data(
@@ -48,5 +38,5 @@ def load_unlabeled_data(
         dataset_name,
         data_files={"unlabeled": "unlabeled_train.csv"},
         features=Features({"text": Value("string")}),
-        split="train",
+        split="unlabeled",
     )
