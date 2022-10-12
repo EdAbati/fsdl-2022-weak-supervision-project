c76fef943af6c4f0ccf580ee938a6f43674041bb
Edoardo Abati <29585319+EdAbati@users.noreply.github.com>
2022-10-12 19:40:10 +0000

    moved scripts in app

--- services/jupyter/src/app/config.py
+++ services/jupyter/src/app/config.py
@@ -1,21 +1,17 @@
-import enum
 from typing import Optional
 
-from pydantic import BaseSettings, HttpUrl, validator
-
-
-class SettingsModeEnum(str, enum.Enum):
-    DEV = "dev"
-    PROD = "prod"
-    TEST = "test"
+from dotenv import load_dotenv
+from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
 
     PROJECT_NAME: str
-    SERVICE_MODE: SettingsModeEnum = SettingsModeEnum.PROD
     HF_TOKEN: Optional[str] = None
     WANDB_API_KEY: Optional[str] = None
 
 
+load_dotenv()
 settings = Settings()
+
+NUM_LABELS = 4
