08b248735c3650ae8a76d6e0f94f515714e6b179
Diego Quintana <daquintanav@gmail.com>
2022-10-14 10:00:23 +0200

    extend CLI to upload traced model to s3 bucket

--- services/jupyter/src/app/config.py
+++ services/jupyter/src/app/config.py
@@ -7,6 +7,9 @@ class Settings(BaseSettings):
 
     HF_TOKEN: Optional[str] = None
     WANDB_API_KEY: Optional[str] = None
+    AWS_REGION: Optional[str] = None
+    AWS_ACCESS_KEY_ID: Optional[str] = None
+    AWS_SECRET_ACCESS_KEY: Optional[str] = None
 
 
 settings = Settings()
